e058aea050b09aa5fa2ba6ef001fe2e26e53d5d5
zhao-sh <u296727088@163.com>
2024-04-29 13:28:25 +0800

    Update Functions.php

--- src/app/Functions.php
+++ src/app/Functions.php
@@ -1,13 +1,6 @@
 <?php
 function logger($message, $echo = true)
 {
-    $dir = getcwd() . '/var/logs/';
-    if (!file_exists($dir)) {
-        mkdir($dir, 0777, true);
-    }
-
-    @error_log(date('Y-m-d H:i:s') . "\t" . $message . PHP_EOL, 3, $dir . date('Ymd') . '.log');
-
     if ($echo) echo $message . PHP_EOL;
 }
 
